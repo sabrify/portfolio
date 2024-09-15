75d28afbcb1206ba93c15e1276266573a11ec4f2
Hamid <sabri0hamid@gmail.com>
2024-09-15 21:56:48 +0100

    second commit

---
+++ lib/posts.ts
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const rootDirectory = path.join(process.cwd(), 'content', 'posts')
+
+export type Post = {
+  metadata: PostMetadata
+  content: string
+}
+
+export type PostMetadata = {
+  title?: string
+  summary?: string
+  image?: string
+  author?: string
+  publishedAt?: string
+  slug: string
+}
+
+export async function getPostBySlug(slug: string): Promise<Post | null> {
+  try {
+    const filePath = path.join(rootDirectory, `${slug}.mdx`)
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
+    const { data, content } = matter(fileContent)
+    return { metadata: { ...data, slug }, content }
+  } catch (error) {
+    return null
+  }
+}
