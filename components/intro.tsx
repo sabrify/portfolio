73a28140036c777c9a8ba441ca7327b871c078fc
crapuscul <sabri0hamid@gmail.com>
2024-09-16 21:32:01 +0100

    mdx-content fix 2

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I'm Hamid.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Hamid.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
         I&#39;m a web developer based in Kenitra, Morocco. with a focus
          on eCommerce solutions and Shopify development.  I love exploring new technologies and sharing
--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,21 +1,28 @@
-import { JSX } from 'react'
-import { highlight } from 'sugar-high'
-import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
+import { JSX } from 'react';
+import { highlight } from 'sugar-high';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc';
+import React, { ReactNode } from 'react';
 
-// Define the Code component with proper typing
+// Define the Code component
 function Code({
   children,
   ...props
-}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { children: string }) {
-  const codeHTML = highlight(children)
+}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { children?: ReactNode }) {
+  // Check if children is a string before passing it to highlight
+  const codeHTML = typeof children === 'string' ? highlight(children) : '';
 
-  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
+}
+
+// Define the custom components with proper typing for MDX
+interface MDXComponents {
+  code: React.ComponentType<React.HTMLProps<HTMLElement>>;
 }
 
 // Custom components for MDX rendering
-const components: Record<string, any> = {
-  code: Code
-}
+const components: MDXComponents = {
+  code: Code,
+};
 
 export default function MDXContent(
   props: JSX.IntrinsicAttributes & MDXRemoteProps
@@ -25,5 +32,5 @@ export default function MDXContent(
       {...props}
       components={{ ...components, ...(props.components || {}) }}
     />
-  )
+  );
 }
