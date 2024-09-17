6d874b082f076702b8ba355328706cfd53e6345a
Hamid <sabri0hamid@gmail.com>
2024-09-17 21:56:08 +0100

    mobile nav barr styling

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -1,15 +1,15 @@
-'use client';
-import Link from "next/link"
-import ThemeToggle from "./theme-toggle"
-import { useMediaQuery } from "@/hooks/use-media-query"
-import { Drawer, DrawerContent, DrawerTrigger } from './ui/drawer';
-import { MenuIcon } from 'lucide-react';
+'use client'
+import Link from 'next/link'
+import ThemeToggle from './theme-toggle'
+import { useMediaQuery } from '@/hooks/use-media-query'
+import { Drawer, DrawerContent, DrawerTrigger } from './ui/drawer'
+import { MenuIcon } from 'lucide-react'
 
-export default function MainMenu(){
-    const isDesktop = useMediaQuery("(min-width: 768px)");
-    return isDesktop ? 
-        <div className="Desktop">
-            <nav className='container flex max-w-5xl items-center justify-between'>
+export default function MainMenu() {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+  return isDesktop ? (
+    <div className='Desktop'>
+      <nav className='container flex max-w-5xl items-center justify-between'>
         <div>
           <Link href='/' className='font-serif text-5xl font-bold'>
             HS
@@ -32,15 +32,32 @@ export default function MainMenu(){
           <ThemeToggle />
         </div>
       </nav>
-        </div>
-     : <div>
-        <Drawer direction="right">
-    <DrawerTrigger>
-    <MenuIcon />
-    </DrawerTrigger>
-    <DrawerContent>
-        crawer content 
-    </DrawerContent>
-        </Drawer>
-     </div>
+    </div>
+  ) : (
+    <div>
+      <Drawer direction='left'>
+        <DrawerTrigger>
+          <nav className='container flex max-w-5xl items-center justify-between'>
+            <MenuIcon />
+            <div>
+              <ThemeToggle />
+            </div>
+          </nav>
+        </DrawerTrigger>
+        <DrawerContent>
+          <ul className='flex flex-col items-start gap-6 px-20 py-8 text-base font-medium text-muted-foreground sm:gap-10'>
+            <li className='transition-colors hover:text-foreground'>
+              <Link href='/posts'>Posts</Link>
+            </li>
+            <li className='transition-colors hover:text-foreground'>
+              <Link href='/projects'>Projects</Link>
+            </li>
+            <li className='transition-colors hover:text-foreground'>
+              <Link href='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  )
 }
