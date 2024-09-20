de532741505e0569443b8d985ac484917c0581d2
Hamid <sabri0hamid@gmail.com>
2024-09-20 21:40:07 +0100

    social media links

--- components/footer.tsx
+++ components/footer.tsx
@@ -41,7 +41,7 @@ const navigation = [
   },
   {
     name: 'X',
-    href: 'https://x.com',
+    href: 'https://x.com/codewithsabri',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path d='M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z' />
@@ -50,7 +50,7 @@ const navigation = [
   },
   {
     name: 'GitHub',
-    href: 'https://github.com',
+    href: 'https://github.com/sabrify',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path
@@ -63,7 +63,7 @@ const navigation = [
   },
   {
     name: 'YouTube',
-    href: 'https://youtube.com',
+    href: 'https://youtube.com/@codewithsabri',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path
@@ -96,7 +96,7 @@ export default function Footer() {
             ))}
           </div>
           <div className='mt-8 md:order-1 md:mt-0'>
-            <p className='text-center text-s leading-5 text-muted-foreground'>
+            <p className='text-s text-center leading-5 text-muted-foreground'>
               &copy; {new Date().getFullYear()} Sabri Hamid. All rights
               reserved.
             </p>
