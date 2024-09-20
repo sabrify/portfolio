e5a614fa2684afc5b0d8cfb214442abb0d5f3222
Hamid <sabri0hamid@gmail.com>
2024-09-20 21:11:56 +0100

    unused imports cleanup

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,5 @@
 import { getPosts } from '@/lib/posts'
 import PostsWithSearch from '@/components/posts-with-search'
-import Posts from '@/components/posts'
 export default async function PostsPage() {
   const posts = await getPosts()
 
@@ -9,7 +8,7 @@ export default async function PostsPage() {
       <div className='container max-w-5xl'>
         <h1 className='title mb-12'>Posts</h1>
 
-        <Posts posts={posts} />
+        <PostsWithSearch posts={posts} />
       </div>
     </section>
   )
